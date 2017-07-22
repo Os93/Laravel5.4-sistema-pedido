a8088148d2bea1b6cbee6a1ac343b22ac82395d9
Jesus Arias <jesus.d.arias96@gmail.com>
2017-07-21 23:17:22 -0600

    Busqueda y edicion de articulos

--- proyecto industria/sp/app/Http/Controllers/ArticuloController.php
+++ proyecto industria/sp/app/Http/Controllers/ArticuloController.php
@@ -5,21 +5,57 @@ namespace sistema_pedidos\Http\Controllers;
 use Illuminate\Http\Request;
 //hacer referencia al modelo
 
+//Hacemos referencia al modelo
+use sistema_pedidos\Tbl_articulo;
+
+//Hacemos referencia a redirect para redireccionar
+use Illuminate\Support\Facades\Redirect;
+
+//Hacemos referencia a request de validacion
+use sistema_pedidos\Http\Requests\ArticuloFormRequest;
+
+//Usar la clase DB de Laravel
+use DB;
 
 
 class ArticuloController extends Controller
 {
     //constructor
     public function __construct(){}
-    
-    
-    
-    public function index(){
-    	return view ("articulos.index");
+
+
+
+    public function index(Request $request){
+      if($request){
+        $query = trim($request->get('searchText'));
+        $articulos = DB::table('tbl_articulo')->where('descripcion','LIKE','%'.
+          $query.'%')
+          ->orderBy('referencia','asc')
+          ->paginate(10);
+        return view('articulos.index', ["articulos"=>$articulos, "searchText"=>
+          $query]);
+      }//return view ("articulos.index");
     }
 
     public function create(){
     	return view ("articulos.create");
     }
-    
+
+    public function show($id){
+      return view("articulos.show",["articulo"=>Tbl_articulo::findOrFail($id)]);
+    }
+
+    public function edit($id){
+        return view("articulos.edit",["articulo"=>Tbl_articulo::findOrFail($id)]);
+    }
+
+    public function update(ArticuloFormRequest $request,$id)
+    {
+        $articulo=Tbl_articulo::findOrFail($id);
+        $articulo->descripcion=$request->get('descripcion');
+        $articulo->precio=$request->get('precio');
+        $articulo->update();
+        return Redirect::to('/articulos');
+    }
+
 }
--- proyecto industria/sp/resources/views/articulos/index.blade.php
+++ proyecto industria/sp/resources/views/articulos/index.blade.php
@@ -1,4 +1,42 @@
 @extends('layouts.admin')
 @section('contenido')
-    <p>indextent.</p>
+    <div class="row">
+      <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+        <h3>Listado de artículos
+          <a href="articulos/create">
+            <button class="btn btn-success" type="button" name="button">
+              Nuevo
+            </button>
+          </a>
+        </h3>
+        @include('articulos.search')
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="table-responsive">
+          <table class="table table-striped table bordered table-condensed
+          table-hover">
+            <thead>
+              <th>Referencia</th>
+              <th>Descripcion</th>
+              <th>Precio</th>
+            </thead>
+            @foreach ($articulos as $articulo)
+              <tr>
+                <td>{{$articulo->referencia}}</td>
+                <td>{{$articulo->descripcion}}</td>
+                <td>{{$articulo->precio}}</td>
+                <td>
+                  <a href="{{URL::action('ArticuloController@edit', $articulo->referencia)}}"><button class="btn btn-info">Editar</button></a>
+                  <a href="#"><button class="btn btn-danger">Eliminar</button></a>
+                </td>
+              </tr>
+            @endforeach
+          </table>
+        </div>
+        {{$articulos->render()}}
+      </div>
+    </div>
+
 @endsection
